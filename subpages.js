b847e613ab31c50a2168a9c40c31fc637277c4e2
Chinmayi-Prasad <chinmayi2398@gmail.com>
2016-10-16 11:04:21 +0530

    Add files via upload

---
+++ subpages.js
@@ -0,0 +1,30 @@
+function FunNorth()
+	{ document.getElementById("north").style.display="block"; }
+	
+function RemNorth()
+	{ document.getElementById("north").style.display="none";}
+	
+
+function FunSouth()
+	{ document.getElementById("south").style.display="block"; }
+	
+function RemSouth()
+	{ document.getElementById("south").style.display="none";}
+
+	function FunCentral()
+	{ document.getElementById("central").style.display="block"; }
+	
+function RemCentral()
+	{ document.getElementById("central").style.display="none";}
+
+	function FunWest()
+	{ document.getElementById("west").style.display="block"; }
+	
+function RemWest()
+	{ document.getElementById("west").style.display="none";}
+
+	function FunEast()
+	{ document.getElementById("east").style.display="block"; }
+	
+function RemEast()
+	{ document.getElementById("east").style.display="none";}
